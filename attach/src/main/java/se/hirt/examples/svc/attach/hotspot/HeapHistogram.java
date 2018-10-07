63c1ddad25a541cad924544d97a4fc998d85cbf0
Marcus Hirt <marcus@hirt.se>
2018-10-08 00:29:48 +0200

    Adding heap histogram

---
+++ attach/src/main/java/se/hirt/examples/svc/attach/hotspot/HeapHistogram.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018 Marcus Hirt
+ *                    www.hirt.se
+ *
+ * This software is free:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESSED OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright (C) Marcus Hirt, 2018
+ */
+package se.hirt.examples.svc.attach.hotspot;
+
+import java.io.IOException;
+
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+
+import se.hirt.examples.svc.attach.util.AttachUtils;
+import sun.tools.attach.HotSpotVirtualMachine;
+
+/**
+ * Attaches to the JVM with the specified PID, and lists a heap histogram.
+ * <p>
+ * Note that we're being nasty little hobbitses with the evil downcast.
+ * 
+ * @author Marcus Hirt
+ */
+@SuppressWarnings("restriction")
+public final class HeapHistogram {
+	public static void main(String[] args) throws AttachNotSupportedException, IOException {
+		AttachUtils.printJVMVersion();
+		String pid = AttachUtils.checkPid(args);
+		VirtualMachine vm = VirtualMachine.attach(pid);
+		HotSpotVirtualMachine hsvm = (HotSpotVirtualMachine) vm;
+		String result = AttachUtils.readFromStream(hsvm.heapHisto(new Object[0]));
+		System.out.println(result);
+		vm.detach();
+	}
+}
